c7a461ee56f7aaa6998bea5b74e731b94f4ce568
Roman Gellert <titangtace@gmail.com>
2024-09-12 15:51:19 +0200

    feat: задание l0 t3

--- l1/t3/src/main.rs
+++ l1/t3/src/main.rs
@@ -1,10 +1,14 @@
+use std::sync::mpsc;
 use std::thread;
 
 fn main() {
     // количество чисел в массиве
-    let n = 100;
+    let n = 50;
     // создание массива
     let vec: Vec<u32> = (0..n).collect();
+
+    // каналы
+    let (tx, rx) = mpsc::channel();
 
     // количество элементов для одного трэда
     let chunk_size = 10;
@@ -19,14 +23,22 @@ fn main() {
         // передача владения частью данных
         let chunk_owned = chunk.to_owned();
 
+        // клонирование канала отправки
+        let tx_clone = tx.clone();
+
         // создания отдельного трэда для печати
         let handle = thread::spawn(move || {
+            // локальная для трэда сумма квадратов
+            let mut sum_of_squares: u32 = 0;
             for num in chunk_owned {
-                println!("{}", num*num);
+                sum_of_squares += num * num;
             }
+
+            // отправка суммы квадратов в канал получения
+            tx_clone.send(sum_of_squares).unwrap();
         });
 
-        // добавления трэда в массив трэдов
+        // добавления хэндлера в массив
         handles.push(handle);
     }
 
@@ -34,4 +46,11 @@ fn main() {
     for handle in handles {
         handle.join().unwrap();
     }
+
+    // удаление оригинального канала отправки
+    drop(tx);
+
+    // обработка финальных данных
+    let sum_of_squares: u32 = rx.iter().sum();
+    println!("{}", sum_of_squares);
 }
